73d77d5a0c3065d4878a2a2bad62a56e9e3ecee9
LanErw <erwanlanglais3@gmail.com>
2024-05-31 10:48:21 +0200

    contact

--- JS/infos_et_contact.js
+++ JS/infos_et_contact.js
@@ -1,13 +1,96 @@
 function buttonDisabler(){
-    let name = document.getElementById("contact-name-field");
-    name.addEventListener("input")
-    let email = document.getElementById("contact-email-field");
-    let com = document.getElementById("contact-comment");
-    name.setAttribute("pattern","`[A-Z][a-z]* [A-Z][a-z]*`");
-    com.setAttribute("minlength",20);
-    com.setAttribute("maxlength",1000);
+    let form = document.getElementById("contact-form");
+    form.addEventListener("input",validContact);
     }
-
+function validContact(event){
+    let name = document.getElementById("contact-name-field").value;
+    let content = document.getElementById("contact-comment").value;
+    let mail = document.getElementById("contact-email-field").value;
+    let isDisabled = document.getElementById("contact-submit-button").hasAttribute("disabled");
+    if(!validName(name)){
+        document.getElementById("invalidName").innerText = "Le Nom Est Invalide ! Il doit ressembler à ça : Nom Prénom";
+    }
+    else{
+        document.getElementById("invalidName").innerText = "";
+    }
+    if(!validMail(mail)){
+        document.getElementById("invalidMail").innerText = "L'Email Est Invalide ! Il doit ressembler à ça : *****@*****.**";
+    }
+    else{
+        document.getElementById("invalidMail").innerText = "";
+    }
+    if(!validContent(content)){
+        document.getElementById("invalidComment").innerText = "Le Commentaire Est Invalide ! Il doit y avoir au moins 20 caractères, mais pas plus de 1000.";
+    }
+    else{
+        document.getElementById("invalidComment").innerText = "";
+    }
+    if(validName(name) && validContent(content) && validMail(mail) &&isDisabled==true){
+        document.getElementById("contact-submit-button").removeAttribute("disabled");
+    }
+    else{
+        if(isDisabled==false){
+            if(!validName(name) || !validContent(content) || !validMail(mail)){
+                document.getElementById("contact-submit-button").setAttribute("disabled","");
+            }
+        }
+    }
+}
+function invalidNameMessage(){
+    document.getElementById("invalidName").innerText = "Le Nom Est Invalide ! Il doit ressembler à ça : Nom Prénom";
+}
+function invalidMailMessage(){
+    document.getElementById("invalidMail").innerText = "L'Email Est Invalide ! Il doit ressembler à ça : *****@*****.**";
+}
+function invalidContentMessage(){
+    document.getElementById("invalidComment").innerText = "Le Commentaire Est Invalide ! Il doit y avoir au moins 20 caractères, mais pas plus de 1000.";
+}
+function validName(name){
+    let strName = name+"";
+    if(strName.length<3){
+        return false;
+    }
+    let nbWords = 1;
+    for(let e of strName){
+        if(e==" "){
+            nbWords++;
+        }
+    }
+    if(nbWords==2){
+        return true;
+    }
+    return false;
+}
+function validMail(mail){
+    let strMail = mail+"";
+    let isAt = 0;
+    let isDot = 0;
+    let isAtBeforeDot = false;
+    for(let elem of strMail){
+        if(elem=="@"){
+            isAt++;
+            if(isDot==0){
+            }
+        }
+        if(elem=="."){
+            isDot++;
+            if(isAt==1){
+                isAtBeforeDot = true;
+            }
+        }
+    }
+    if(isAt==1 && isDot==1 && isAtBeforeDot){
+        return true;
+    }
+    return false;
+}
+function validContent(content){
+    let strContent = content+"";
+    if(strContent.length>=20 && strContent.length<=1000){
+        return true;
+    }
+    return false;
+}
 function main(){
     buttonDisabler();
 }
